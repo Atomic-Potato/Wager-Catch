458ec98e0f92254fc384cca5df02f9ff30924e24
Atomic Potato <medyanmehiddine@gmail.com>
2023-11-28 13:47:46 +0200

    Fixed can place on safe areas

--- Assets/Scripts/GridPlacementManager.cs
+++ Assets/Scripts/GridPlacementManager.cs
@@ -3,10 +3,10 @@ using Pathfinding;
 
 public class GridPlacementManager : MonoBehaviour
 {
-    [SerializeField] Pathfinding.Grid placementGrid;
-    [SerializeField] Pathfinding.Grid unitsGrid;
-    [SerializeField] GameObject objectToSpawn;
-    [SerializeField] LayerMask unitsLayer;
+    [SerializeField] Pathfinding.Grid _placementGrid;
+    [SerializeField] Pathfinding.Grid _unitsGrid;
+    [SerializeField] GameObject _objectToSpawn;
+    [SerializeField] LayerMask _invalidPlacementLayers;
 
     void Update()
     {
@@ -17,24 +17,24 @@ public class GridPlacementManager : MonoBehaviour
     void PlaceObject()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Node placementNode = placementGrid.GetNodeFromWorldPosition(mousePosition);
+        Node placementNode = _placementGrid.GetNodeFromWorldPosition(mousePosition);
 
         if (!IsCanPlaceOnNode())
             return;
 
-        GameObject spawnedObject = Instantiate(objectToSpawn, placementNode.WorldPosition, Quaternion.identity);
+        GameObject spawnedObject = Instantiate(_objectToSpawn, placementNode.WorldPosition, Quaternion.identity);
         BoxCollider2D collider = spawnedObject.GetComponent<BoxCollider2D>();
 
         if (collider != null)
         {
-            unitsGrid.UpdateGridSection(collider.bounds.min, collider.bounds.max, placementNode.IsSafe, false);
-            placementGrid.UpdateGridSection(collider.bounds.min, collider.bounds.max, placementNode.IsSafe, false);
+            _unitsGrid.UpdateGridSection(collider.bounds.min, collider.bounds.max, placementNode.IsSafe, false);
+            _placementGrid.UpdateGridSection(collider.bounds.min, collider.bounds.max, placementNode.IsSafe, false);
         }
         
         bool IsCanPlaceOnNode()
         {
-            BoxCollider2D toSpawnCollider = objectToSpawn.GetComponent<BoxCollider2D>();
-            bool isAgentOnNode = Physics2D.OverlapBox(placementNode.WorldPosition, toSpawnCollider.size, 0f, unitsLayer) != null;
+            BoxCollider2D toSpawnCollider = _objectToSpawn.GetComponent<BoxCollider2D>();
+            bool isAgentOnNode = Physics2D.OverlapBox(placementNode.WorldPosition, toSpawnCollider.size, 0f, _invalidPlacementLayers) != null;
             return !placementNode.IsSafe && placementNode.IsWalkable && placementNode.IsEditable && !isAgentOnNode;
         }
     }
