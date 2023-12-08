63f889afc5170a51450790b116458e4f98b82f97
Atomic Potato <medyanmehiddine@gmail.com>
2023-12-08 16:16:33 +0200

    Dynamic Icons script

---
+++ Assets/Scripts/Other/DynamicIcon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class DynamicIcon : MonoBehaviour
+{
+    [SerializeField] Sprite _catchersIcon;
+    [SerializeField] Sprite _runnersIcon;
+    [SerializeField] Sprite _nuteralIcon;
+
+    [Space]
+    [SerializeField] SpriteRenderer[] _iconRenderes;
+
+    void Start()
+    {
+        Sprite icon; 
+        switch (GameManager.Instance.PlayerTeam)
+        {
+            case TagsManager.Tag.Catcher:
+                icon = _catchersIcon;
+                break;
+            case TagsManager.Tag.Runner :
+                icon = _runnersIcon;
+                break;
+            default:
+                icon = _nuteralIcon;
+                break;
+        }
+        
+        foreach (SpriteRenderer renderer in _iconRenderes)
+            renderer.sprite = icon;
+    }
+}
