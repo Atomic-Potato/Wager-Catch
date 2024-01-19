106bee876864c48e9f3e5492b6bb75dfc3439ea0
Atomic Potato <medyanmehiddine@gmail.com>
2024-01-19 11:44:23 +0200

    Running trail

---
+++ Assets/Scripts/Other/TrailParticlesManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class TrailParticlesManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _trailEffect;
+    [SerializeField] TeamPlayer _teamPlayer;
+    [SerializeField] Player _player;
+
+    ParticleSystem.MinMaxCurve _originalRateOverTime;
+
+    void Awake()
+    {
+        _originalRateOverTime = _trailEffect.emission.rateOverTime;
+    }
+
+    void Update()
+    {
+        if (!_teamPlayer && !_player)
+            return;
+            
+        bool isMoving = _teamPlayer ? _teamPlayer.IsMoving : _player.IsMoving;
+        if (isMoving)
+        {
+            var emission = _trailEffect.emission;
+            emission.rateOverTime = _originalRateOverTime;
+        }
+        else
+        {
+            var emission = _trailEffect.emission;
+            emission.rateOverTime = 0f;
+        }
+    }
+}
