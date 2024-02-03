75c5ca72bf933c98b4a2e73e97aa5c3e8d9eb071
Atomic Potato <medyanmehiddine@gmail.com>
2024-02-03 17:54:43 +0200

    Created a destroy manager

---
+++ Assets/Scripts/Managers/DestroyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyManager : Singleton<DestroyManager>
+{
+    public void Destroy(Object obj, float delay = 0, bool isRealTimeDelay = false)
+    {
+        if (isRealTimeDelay)
+            StartCoroutine(DestroyRealTime(obj, delay));
+        else
+            Destroy(obj, delay);
+    }
+
+    IEnumerator DestroyRealTime(Object obj, float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        GameObject.Destroy(obj); 
+    }
+}
