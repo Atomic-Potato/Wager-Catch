604453c0cab0036aa859948e633e970fe88c62b0
Atomic Potato <medyanmehiddine@gmail.com>
2024-02-03 20:44:36 +0200

    Speed up SFX

---
+++ Assets/Scripts/Audio/AudioPauseHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPauseHandler : MonoBehaviour
+{
+    [SerializeField] AudioSource _source;
+
+    void OnEnable()
+    {
+        _source.Pause();
+    }
+
+    void OnDisable()
+    {
+        _source.UnPause();
+    }
+}
--- Assets/Scripts/Other/TimeSpeedUp.cs
+++ Assets/Scripts/Other/TimeSpeedUp.cs
@@ -4,6 +4,7 @@ using UnityEngine.EventSystems;
 public class TimeSpeedUp : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField, Min(1f)] float _timeScaleMultiplier = 3.5f;
+    [SerializeField] GameObject _sound;
 
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -11,6 +12,8 @@ public class TimeSpeedUp : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             return;
         TimeScaleManipulator.Instance.ChangeScale(_timeScaleMultiplier);
         SoundManager.Instance.PlaySoundAtPosition(Vector2.zero, SoundManager.Sound.Click_1, false, false);
+        if (_sound != null)
+            _sound.SetActive(true);
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -19,5 +22,7 @@ public class TimeSpeedUp : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             return;
         TimeScaleManipulator.Instance.RestoreTime();
         SoundManager.Instance.PlaySoundAtPosition(Vector2.zero, SoundManager.Sound.Click_2, false, false);
+        if (_sound != null)
+            _sound.SetActive(false);
     }
 }
