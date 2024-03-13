effbfe30b91b5be12a6c64a8d7f86372456a57da
Atomic Potato <medyanmehiddine@gmail.com>
2024-03-13 10:49:30 +0200

    Node documentation

--- Assets/Scripts/Pathfinding/Node.cs
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -3,19 +3,47 @@ using UnityEngine;
 
 namespace Pathfinding
 {
+    /// <summary>
+    /// Nodes to fill the Grid class with which can be marked as either walkable or unwalkable
+    /// </summary>
     public class Node : IHeapItem<Node>
     {
+        /// <summary>
+        /// Connects the node with another node so the path can be retraced back 
+        /// from this node back to the path starting node
+        /// </summary>
         public Node Parent;
 
+        /// <summary>
+        /// The node horizontal index in the grid 
+        /// </summary>
         public int GridPositionX;
+        /// <summary>
+        /// The node vertical index in the grid
+        /// </summary>
         public int GridPositionY;
+        /// <summary>
+        /// The horizontal and vertical indices of the node inside the grid
+        /// </summary>
         public Vector2 GridPosition;
         
-        public int G_Cost; // distance from starting node
-        public int H_Cost; // distance from end node
+        /// <summary>
+        /// distance from starting node
+        /// </summary>
+        public int G_Cost;
+        /// <summary>
+        /// distance from end node
+        /// </summary>
+        public int H_Cost;
+        /// <summary>
+        /// H_Cost + G_Cost
+        /// </summary>
         public int F_Cost => H_Cost + G_Cost; 
 
         int _heapIndex;
+        /// <summary>
+        /// The index of the node in the heap tree. Used to optimize the node search performance in A*
+        /// </summary>
         public int HeapIndex
         {
             get
@@ -27,9 +55,18 @@ namespace Pathfinding
                 _heapIndex = value;
             }
         }
-
+        
+        /// <summary>
+        /// Can the agent walk on this node / will it be avoided in the A* algorithm
+        /// </summary>
         public bool IsWalkable;
+        /// <summary>
+        /// The world cooridnates of the node (Use GridPosition to get the node indices in the grid)
+        /// </summary>
         public Vector2 WorldPosition;
+        /// <summary>
+        /// The surrounding nodes (Usually 8 nodes unless the node is on the edge of the grid)
+        /// </summary>
         public List<Node> Neighboors;
 
         public Node(bool isWalkable, Vector2 worldPosition, int gridPositionX, int gridPositionY)
